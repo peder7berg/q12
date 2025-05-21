e53129701f4daa8fed5be4d92e995293f9eb1816
peder7berg <peder.sjoberg@jeppis.fi>
2025-05-21 17:33:55 +0300

    Add files via upload

---
+++ fragor.js
@@ -0,0 +1,36 @@
+const fragor = [
+  {
+    titel: "Vilka är huvudstäder?",
+    alternativ: [
+      { label: "Stockholm", answer: "ja" },
+      { label: "Göteborg", answer: "nej" },
+      { label: "Oslo", answer: "ja" },
+      { label: "Malmö", answer: "nej" },
+      { label: "Helsingfors", answer: "ja" },
+      { label: "Umeå", answer: "nej" },
+      { label: "Köpenhamn", answer: "ja" },
+      { label: "Lund", answer: "nej" },
+      { label: "Reykjavik", answer: "ja" },
+      { label: "Åbo", answer: "nej" },
+      { label: "Berlin", answer: "ja" },
+      { label: "Nice", answer: "nej" }
+    ]
+  },
+  {
+    titel: "Vilka är däggdjur?",
+    alternativ: [
+      { label: "Hund", answer: "ja" },
+      { label: "Fisk", answer: "nej" },
+      { label: "Katt", answer: "ja" },
+      { label: "Fågel", answer: "nej" },
+      { label: "Elefant", answer: "ja" },
+      { label: "Orm", answer: "nej" },
+      { label: "Häst", answer: "ja" },
+      { label: "Groda", answer: "nej" },
+      { label: "Lejon", answer: "ja" },
+      { label: "Fjäril", answer: "nej" },
+      { label: "Kanin", answer: "ja" },
+      { label: "Sköldpadda", answer: "nej" }
+    ]
+  }
+];
